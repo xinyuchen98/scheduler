8fd6ca84ce791ed5f9ad58eff2f602a888a176b9
Xinyu Chen <xinyuchen98@gmail.com>
2022-03-06 21:29:18 -0700

    Add tests and fixed spots text

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,10 +5,21 @@ import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
   let dayListItemClass = classNames("day-list__item", {"day-list__item--selected": props.selected, "day-list__item--full": (props.spots === 0)});
+
+  const formatSpots = function(spots) {
+    if (spots === 0) {
+      return `no spots`;
+    } else if (spots === 1) {
+      return `1 spot`;
+    } else {
+      return `${spots} spots`;
+    }
+  }
+
   return (
     <li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)} remaining</h3>
     </li>
   );
 }
