2da5bde76351d061311f757e1e536238705a14d9
Xinyu Chen <xinyuchen98@gmail.com>
2022-03-06 19:49:21 -0700

    Refactored button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,10 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   let buttonClass = "button";
- 
-   if (props.confirm) {
-     buttonClass += " button--confirm";
-   }
- 
-   if (props.danger) {
-     buttonClass += " button--danger";
-   }
+   let buttonClass = classNames("button", {"button--confirm": props.confirm, "button--danger": props.danger});
  
    return <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>{props.children}</button>;
  }
