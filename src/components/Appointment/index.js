7e8ae1d72d7d7d15601b0832cd41d2a185c71225
Xinyu Chen <xinyuchen98@gmail.com>
2022-03-07 20:22:33 -0700

    Displaying appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
-  const formatAppointment = function(time) {
-    if (!time) {
-      return "No appointments";
-    } else {
-      return `Appointment at ${time}`;
-    }
-  }
-
   return (
-    <article className="appointment">{formatAppointment(props.time)}</article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+    </article>
   );
 }
