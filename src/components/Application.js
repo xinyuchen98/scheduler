33c9ddf43d8a1c9cd0527f47508b6a725869583d
Xinyu Chen <xinyuchen98@gmail.com>
2022-03-14 19:48:21 -0600

    Combine states in application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,51 +5,19 @@ import "components/Application.scss";
 import DayList from "components/DayList"
 import Appointment from "components/Appointment";
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
-
 export default function Application(props) {
-  const [currentDay, setCurrentDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState(prev => ({ ...prev, day }));
+
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
-    axios.get("/api/days").then((response => setDays(response.data)))
+    axios.get("/api/days").then(response => setDays(response.data));
   }, []);
 
   return (
@@ -63,9 +31,9 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={currentDay}
-            onChange={setCurrentDay}
+            days={state.days}
+            value={state.day}
+            onChange={setDay}
           />  
         </nav>
         <img
@@ -75,7 +43,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {Object.values(appointments).map((appointment) => {
+        {Object.values(state.appointments).map((appointment) => {
           return (
             <Appointment
               key={appointment.id}
