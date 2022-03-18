a94bcb899db40e750451e0730446a0805ab45771
Xinyu Chen <xinyuchen98@gmail.com>
2022-03-18 01:12:55 -0600

    Added comments for custom hook useVisualMode (Appointment)

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,11 +1,16 @@
+// custom hook for switching states of Appointment components
 import { useState } from "react";
 
 export default function useVisualMode(initialMode) {
+  // current mode of the component
   const [mode, setMode] = useState(initialMode);
+  // array of mode history
   const [history, setHistory] = useState([initialMode]);
 
+  // switch to a new mode from current mode
   const transition = function(newMode, replace = false) {
     setMode(newMode);
+    // optional replace parameter set to true then replace the current mode in history
     if (!replace) {
       setHistory(prev => [...prev, newMode]);
     } else {
@@ -13,6 +18,7 @@ export default function useVisualMode(initialMode) {
     }
   }
 
+  // go back to the previous mode in history
   const back = function() {
     if (history.length > 1) {
       setMode(history[history.length - 2]);
@@ -20,5 +26,6 @@ export default function useVisualMode(initialMode) {
     }
   }
 
+  // return the value of current mode and methods to switch to new mode and go back to previous mode
   return { mode, transition, back };
 }
