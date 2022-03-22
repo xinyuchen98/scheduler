7016d06813f24c35dc60ffa91f26c14c3cbe13f0
Xinyu Chen <xinyuchen98@gmail.com>
2022-03-22 13:59:17 -0600

    Comments to Navigation Cypress test

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,12 +1,15 @@
+// Navigation Cypress tests
 describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+  
   it("should navigate to Tuesday", () => {
     cy.visit("/");
   
+    // Find and click on Tuesday
     cy.contains("[data-testid=day]", "Tuesday")
       .click()
-      .should("have.class", "day-list__item--selected")
+      .should("have.class", "day-list__item--selected")  // Check if it has Tuesday selected
   });
 });
