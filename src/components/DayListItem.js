7bdd3b8551052056e92f41710d46df260c566160
Xinyu Chen <xinyuchen98@gmail.com>
2022-03-22 12:47:36 -0600

    Added test-id to DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -17,7 +17,7 @@ export default function DayListItem(props) {
   }
 
   return (
-    <li className={dayListItemClass} onClick={() => props.setDay(props.name)} selected={props.selected}>
+    <li className={dayListItemClass} onClick={() => props.setDay(props.name)} selected={props.selected} data-testid="day">
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{formatSpots(props.spots)} remaining</h3>
     </li>
